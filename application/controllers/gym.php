9e4912efb0127a9fd1433ada3bbf694c5fb156ba
mark p <markmayalla@gmail.com>
2018-03-22 17:34:30 +0300

    Worked with GYM RAtes

--- application/controllers/gym.php
+++ application/controllers/gym.php
@@ -169,124 +169,266 @@ class Gym extends CI_Controller {
 
 
     }
-    //this function is for gym groups
-       public function gym_rates(){
-    
-            //passing the URi segmemnts
-        $uri_segment3 = $this->uri->segment(3);
-        $uri_segment4 = $this->uri->segment(4);
+
+    //The Gym Rates Controller Method
+
+    public function gym_rates() {
+
+        //INstatiating Uri Segments
+        $uriSegment3 = $this->uri->segment(3);
+        $uriSegment4 = $this->uri->segment(4);
+        $uriSegment5 = $this->uri->segment(5);
+        $uriSegment6 = $this->uri->segment(6);
 
 
-        //loading content models
+        //Loading Impontant Models
         $this->load->model('gym_model');
-        $this->load->model('gym_rate_group_model');
-       
-           if ($uri_segment4 == "insert") {
-            $formData = array();
-            $formData['gym_id'] = $uri_segment3;
-            $formData['gym_groupname'] = strtolower($_POST['gym_groupname']);
-            $this->gym_rate_group_model->insert($formData);
-            
-        }
-              
-         
-          $data['success_msg'] = "Added Successfull";
-        //getting gym group name corresponding gym id
-        $gym_rate_group = $this->gym_rate_group_model->get_many_by(array('gym_id' => $uri_segment3));
-       $data['gym_rate_group'] = $gym_rate_group;
-         //gym_get selcect option value 
-        $gym_option = $this->gym_rate_group_model->get_many_by(array('gym_id' => $uri_segment3));
-       $data['gym_option'] = $gym_option;
-
-         $gym= $this->gym_model->get($uri_segment3);
-         $data['gym'] = $gym;
-         $data['main_content'] = "gym_rates";
-         $data['active'] = "gym_rates";
-         $this->load->view('includes/gym' ,$data);
-    }
-        //this fucntion if for duration bundle 
-        public function gym_bundle(){
-    
-            //passing the URi segmemnts
-        $uri_segment3 = $this->uri->segment(3);
-        $uri_segment4 = $this->uri->segment(4);
-    
-
-        //loading content models
-        $this->load->model('gym_model');
-        $this->load->model('gym_rate_duration_bundle_model');
-       
-           if ($uri_segment4 == "insert") {
-            $formData = array();
-            $formData['gym_id'] = $uri_segment3;
-            $formData['bundle_name'] = strtolower($_POST['bundle_name']);
-            $this->gym_rate_duration_bundle_model->insert($formData);
-            
-        }
-              
-         
-          $data['success_msg'] = "Added Successfull";
-         //getting gym duration bundle corresponding gym id
-        $gym_rate_bundle = $this->gym_rate_duration_bundle_model->get_many_by(array('gym_id' => $uri_segment3));
-       $data['gym_rate_bundle'] = $gym_rate_bundle;
-       //get data from option value 
-        $select = $this->gym_rate_duration_bundle_model->get_many_by(array('gym_id' => $uri_segment3));
-       $data['select'] = $select;
-
-         $gym= $this->gym_model->get($uri_segment3);
-         $data['gym'] = $gym;
-         $data['main_content'] = "gym_rates";
-         $data['active'] = "gym_rates";
-         $this->load->view('includes/gym' ,$data);
-    }
-        //this function is for amount for each duration bundle 
-        public function gym_rate_amount(){
-    
-            //passing the URi segmemnts
-        $uri_segment3 = $this->uri->segment(3);
-        $uri_segment4 = $this->uri->segment(4);
-        $uri_segment5  = $this->uri->segment(5);
-    
-
-        //loading content models
-        $this->load->model('gym_model');
-        $this->load->model('gym_rate_duration_bundle_model');
-        $this->load->model('gym_rate_group_model');
         $this->load->model('gym_rate_model');
-     
+        $this->load->model('gym_rate_group_model');
+        $this->load->model('gym_rate_duration_bundle_model');
 
-       
-           if ($uri_segment4 == "insert") {
-        $gym = $this->gym_rate_group_model->get_by(array('id' => $_POST['id']));
-        $gym = $this->gym_rate_duration_bundle_model->get_by(array('id' => $_POST['id']));
 
-            $formData = array();
-            $formData['gym_id'] = $uri_segment3;
-            $formData['amount'] = strtolower($_POST['amount']);
-            $this->gym_rate_model->insert($formData);
-            
+        if ($uriSegment4 == 'groups' || $uriSegment4 == false) {
+
+            if ($uriSegment5 == 'insert' || $uriSegment5 == false) {
+                $data['form'] = array(
+                    'action' => base_url() . "index.php/gym/gym_rates/" . $uriSegment3 . '/groups/insert',
+                    'btn_name' => 'add_group',
+                    'btn_value' => 'Add Group'
+                );
+
+                if (isset($_POST['add_group'])) {
+                    $formData = array();
+                    foreach ($_POST as $key => $value) {
+                        if ($key != 'add_group') {
+                            $formData[$key] = $value;
+                        }
+                    }
+                    $formData['gym_id'] = $uriSegment3;
+                    $this->gym_rate_group_model->insert($formData);
+                    $data['success_msg'] = 'Group Added Successfully';
+                }
+
+            }
+
+            if ($uriSegment5 == 'update') {
+                $data['form'] = array(
+                    'action' => base_url() . "index.php/gym/gym_rates/" . $uriSegment3 . '/groups/update/' . $uriSegment6,
+                    'btn_name' => 'edit_group',
+                    'btn_value' => 'Edit Group'
+                );
+                $form_values = $this->gym_rate_group_model->get($uriSegment6);
+                $data['form_values'] = $form_values;
+
+                if (isset($_POST['edit_group'])) {
+                    $formData = array();
+                    foreach ($_POST as $key => $value) {
+                        if ($key != 'edit_group') {
+                            $formData[$key] = $value;
+                        }
+                    }
+
+                    $this->gym_rate_group_model->update($uriSegment6, $formData);
+                    $data['success_msg'] = 'Group Updated Successfully';
+                }
+
+
+
+            }
+
+            if ($uriSegment5 == 'delete') {
+                $data['form'] = array(
+                    'action' => base_url() . "index.php/gym/gym_rates/" . $uriSegment3 . '/groups/insert',
+                    'btn_name' => 'add_group',
+                    'btn_value' => 'Add Group'
+                );
+                $this->gym_rate_group_model->delete($uriSegment6);
+                $this->gym_rate_model->delete_by(array('group_id' => $uriSegment6));
+            }
+
+            $result = $this->gym_rate_group_model->get_many_by(array('gym_id' => $uriSegment3));
+
+            if (count($result) > 0) {
+                $data['groups'] = $result;
+            }
+
+            $data['sub_content'] = 'groups';
+
+
         }
 
-         if($uri_segment4 =='delete_rate_amount'){
+        if ($uriSegment4 == 'bundles') {
+            if ($uriSegment5 == 'insert' || $uriSegment5 == false) {
+                $data['form'] = array(
+                    'action' => base_url() . "index.php/gym/gym_rates/" . $uriSegment3 . '/bundles/insert',
+                    'btn_name' => 'add_bundle',
+                    'btn_value' => 'Add Bundle'
+                );
 
-            //$this->gym_rate_model->delete_by(array('id' => $uriSegment5));
-             $this->gym_rate_model->delete($uriSegment5);
-                $data['success_msg'] = "Deleted  is successfull";
+                if (isset($_POST['add_bundle'])) {
+                    $formData = array();
+                    foreach ($_POST as $key => $value) {
+                        if ($key != 'add_bundle') {
+                            $formData[$key] = $value;
+                        }
+                    }
+                    $formData['gym_id'] = $uriSegment3;
+                    $this->gym_rate_duration_bundle_model->insert($formData);
+                    $data['success_msg'] = 'Bundle Added Successfully';
+                }
 
-         }
-              
-         
-          $data['success_msg'] = "Added Successfull";
-         
-        //gym rates amount for each bundle and group match to gym ID
-        $gym_amount = $this->gym_rate_model->get_many_by(array('gym_id' => $uri_segment3));
-        $data['gym_amount'] = $gym_amount; 
-         $gym= $this->gym_model->get($uri_segment3);
-         $data['gym'] = $gym;
-         $data['main_content'] = "gym_rates";
-         $data['active'] = "gym_rates";
-         $this->load->view('includes/gym' ,$data);
+            }
+
+            if ($uriSegment5 == 'update') {
+                $data['form'] = array(
+                    'action' => base_url() . "index.php/gym/gym_rates/" . $uriSegment3 . '/bundles/update/' . $uriSegment6,
+                    'btn_name' => 'edit_bundle',
+                    'btn_value' => 'Edit Bundle'
+                );
+                $form_values = $this->gym_rate_duration_bundle_model->get($uriSegment6);
+                $data['form_values'] = $form_values;
+
+                if (isset($_POST['edit_bundle'])) {
+                    $formData = array();
+                    foreach ($_POST as $key => $value) {
+                        if ($key != 'edit_bundle') {
+                            $formData[$key] = $value;
+                        }
+                    }
+
+                    $this->gym_rate_duration_bundle_model->update($uriSegment6, $formData);
+                    $data['success_msg'] = 'Bundle Updated Successfully';
+                }
+
+
+
+            }
+
+            if ($uriSegment5 == 'delete') {
+                $data['form'] = array(
+                    'action' => base_url() . "index.php/gym/gym_rates/" . $uriSegment3 . '/bundles/insert',
+                    'btn_name' => 'add_bundle',
+                    'btn_value' => 'Add bundle'
+                );
+                $this->gym_rate_duration_bundle_model->delete($uriSegment6);
+                $this->gym_rate_model->delete_by(array('bundle_id' => $uriSegment6));
+            }
+
+            $result = $this->gym_rate_duration_bundle_model->get_many_by(array('gym_id' => $uriSegment3));
+
+            if (count($result) > 0) {
+                $data['bundles'] = $result;
+            }
+
+            $data['sub_content'] = 'bundles';
+        }
+
+        if ($uriSegment4 == 'rates') {
+
+            if ($uriSegment5 == 'insert' || $uriSegment5 == false) {
+                $data['form'] = array(
+                    'action' => base_url() . "index.php/gym/gym_rates/" . $uriSegment3 . '/rates/insert/',
+                    'btn_name' => 'add_rate',
+                    'btn_value' => 'Add Rate'
+                );
+
+                if (isset($_POST['add_rate'])) {
+                    $formData = array();
+
+                    $flag = true;
+                    foreach ($_POST as $key => $value) {
+                        if ($key != 'add_rate') {
+                            $formData[$key] = $value;
+                        }
+                    }
+                    $group = $this->input->post('group_id');
+                    $bundle = $this->input->post('bundle_id');
+
+                    $validator = $this->gym_rate_model->get_many_by(array('gym_id' => $uriSegment3));
+                    if (isset($validator) && count($validator) > 0) {
+                        ChromePhp::log($validator);
+                        foreach ($validator as $value) {
+                            if ($value->group_id == $group
+                                && $value->bundle_id == $bundle) {
+                                $flag = false;
+                            }
+                        }
+                    }
+
+                    $formData['gym_id'] = $uriSegment3;
+                    if ($flag) {
+                        $this->gym_rate_model->insert($formData);
+                        $data['success_msg'] = "Gym Rate Added Successfully";
+                    } else {
+                        $data['error_msg'] = "Group Bundle Pair Exists";
+                    }
+
+                }
+            }
+
+            if ($uriSegment5 == 'update') {
+                $data['form'] = array(
+                    'action' => base_url() . "index.php/gym/gym_rates/" . $uriSegment3 . '/rates/update/' . $uriSegment6,
+                    'btn_name' => 'edit_rate',
+                    'btn_value' => 'Edit Rate'
+                );
+
+                $form_values = $this->gym_rate_model->get($uriSegment6);
+                $data['form_values'] = $form_values;
+
+                if (isset($_POST['edit_rate'])) {
+                    $formData = array();
+                    foreach ($_POST as $key => $value) {
+                        if ($key != 'edit_rate') {
+                            $formData[$key] = $value;
+                        }
+                    }
+
+                    $this->gym_rate_model->update($uriSegment6, $formData);
+                    $data['success_msg'] = "Gym Rate Edited Successfully";
+                }
+            }
+
+            if ($uriSegment5 == 'delete') {
+                $data['form'] = array(
+                    'action' => base_url() . "index.php/gym/gym_rates/" . $uriSegment3 . '/rates/insert/',
+                    'btn_name' => 'add_rate',
+                    'btn_value' => 'Add Rate'
+                );
+
+                $this->gym_rate_model->delete($uriSegment6);
+            }
+
+            $groups = $this->gym_rate_group_model->get_many_by(array('gym_id' => $uriSegment3));
+            $bundles = $this->gym_rate_duration_bundle_model->get_many_by(array('gym_id' => $uriSegment3));
+            $rates = $this->gym_rate_model->get_many_by(array('gym_id' => $uriSegment3));
+
+            $groupsView = array();
+            $bundlesView = array();
+            if (isset($rates) && count($rates) > 0) {
+                foreach ($rates as $key => $value) {
+                    $groupsView[$key] = $this->gym_rate_group_model->get($value->group_id);
+                    $bundlesView[$key] = $this->gym_rate_duration_bundle_model->get($value->bundle_id);
+                }
+            }
+            $data['groupsView'] = $groupsView;
+            $data['bundlesView'] = $bundlesView;
+            $data['rates'] = $rates;
+            $data['groups'] = $groups;
+            $data['bundles'] = $bundles;
+            $data['sub_content'] = 'rates';
+        }
+
+        $gym = $this->gym_model->get($uriSegment3);
+        $data['gym'] = $gym;
+        $data['main_content'] = 'gym_rates';
+        $data['active'] = 'gym_rates';
+        $this->load->view('includes/gym', $data);
+
+
     }
+
+
 
 }
 
