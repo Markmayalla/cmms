b216a9bfe2aa06c9db98214ffb7d8846a5e25590
mark p <markmayalla@gmail.com>
2018-05-14 12:01:30 +0300

    Working with Admin Dashboard

--- application/views/includes/page_start2.php
+++ application/views/includes/page_start2.php
@@ -1,9 +1,8 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>WellnessTz | Dashboard</title>
+    <title>CMMS | <?PHP $bool = isset($page_title); echo ($bool == true ? $page_title : ''); ?></title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- bootstrap 3.0.2 -->
     <link href="<?PHP echo base_url(); ?>AdminLTE/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
@@ -24,20 +23,15 @@
     <!-- Theme style -->
     <link href="<?PHP echo base_url(); ?>AdminLTE/css/AdminLTE.css" rel="stylesheet" type="text/css" />
     <!-- My Custome CSS -->
-    <link href="<?PHP echo base_url(); ?>css/custome.css" rel="stylesheet" type="text/css" />
+    <!-- <link href="<?PHP echo base_url(); ?>css/custome.css" rel="stylesheet" type="text/css" /> -->
+    <link href="<?PHP echo base_url(); ?>css/system.css" rel="stylesheet" type="text/css" />
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
 
-    <!-- ChartJS.js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js"></script>
 
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
-
 </head>
-<body class="skin-black">
-<input type="hidden" name="session_email" id="session_email" value="<?PHP echo $this->session->userdata('email'); ?>" />
-<div id="#video_out"></div>
-<?PHP $this->load->view('includes/form_generator.php'); ?>
+<body class="system_body">
