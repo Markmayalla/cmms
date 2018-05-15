954db303c0556ee6eedc5fd992eb3efb7994a7d1
hemmy6894 <37750218+hemmy6894@users.noreply.github.com>
2018-05-15 07:46:43 +0300

    Revert "Working with Admin Dashboard"

--- application/views/includes/system.php
+++
@@ -1,6 +0,0 @@
-<?PHP $this->load->view('includes/page_start2.php'); ?>
-<?PHP
-$this->load->view('includes/system_header');
-$this->load->view('includes/system_footer');
-?>
-<?PHP $this->load->view('includes/page_end2.php'); ?>
