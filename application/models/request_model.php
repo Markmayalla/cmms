bbe456e9645cdda3e9c7d45c10bb52b3ee1be74f
hemmy6894 <37750218+hemmy6894@users.noreply.github.com>
2018-06-21 13:10:22 +0300

    Display requests

--- application/models/request_model.php
+++ application/models/request_model.php
@@ -56,17 +56,18 @@ class request_model extends MY_Model {
 		$request = $this->get_all_requests();
 		$i = 0;
 		foreach($request as $key){
-			$account = $this->account_model->get_by($key->account_id)->users_id;
-			$orgname = $this->organization_model->get_by($key->organizations_has_assets_organizations_id)->name;
-			$asset = $this->asset_model->get_by($key->organizations_has_assets_assets_id)->name;
-			$username = $this->user_model->get_by($account);
+			$account = $this->account_model->get($key->accounts_id)->user_id;
+			$orgname = $this->organization_model->get($key->organizations_has_assets_organizations_id);
+			$asset = $this->asset_model->get($key->organizations_has_assets_assets_id);
+			$username = $this->user_model->get($account);
 			
-			$data[$i]['account_name'] = $username;
+			$data[$i]['username'] = $username;
 			$data[$i]['organization'] = $orgname;
 			$data[$i]['asset'] = $asset;
+			$data[$i]['request'] = $key;
 			$i++;
 		}
 		
-		return $data;
+		return (object)$data;
 	}
 }
--- application/views/dashboard/requests/download.php
+++ application/views/dashboard/requests/download.php
@@ -1,20 +1,43 @@
 <div class="row" style="margin-top:25px;">
 	<div class="col-md-12">
 		<?php
-			$num_user = count($display);
+			//$num_user = count((array)$display);
 			$to_delete = $data['data'];
-			$this->table->set_heading('Equipment ID', 'Equipment Name', 'Price','Quantity', 'Options');
-			for($i = 0; $i < $num_user; $i++){
-				$data = $display[$i];
-				//print_r($data);
-                $edit_btn = '<a href="#" class="btn btn-sm btn-info"><span class="fa fa-pencil"></span> </a>';
-                $delete_btn = '<a href="'.site_url().'/action/delete_item/'.$to_delete.'/'.$data->equipment_id.'" class="btn btn-sm btn-danger"><span class="fa fa-trash-o"></span> </a>';
+			$this->table->set_heading('User', 'Organization Name', 'Asset Name','Status','Option');
+			/*
+				for($i = 0; $i < $num_user; $i++){
+					$data = $display[$i];
+					//print_r($data);
+					$edit_btn = '<a href="#" class="btn btn-sm btn-info"><span class="fa fa-pencil"></span> </a>';
+					$delete_btn = '<a href="'.site_url().'/action/delete_item/'.$to_delete.'/'.$data->equipment_id.'" class="btn btn-sm btn-danger"><span class="fa fa-trash-o"></span> </a>';
+					
+					$cookie_name = "facebook_google_key_value";
+					$cookie_value = "equipment_id";
+					setcookie($cookie_name, $cookie_value, time() + (90), "/");
+					
+					$this->table->add_row($data->equipment_id, $data->equipment_name,$data->unit_price,$data->quantity, $edit_btn . $delete_btn);
+				}
+			*/
+			foreach($display as $data){
+				$account = $data['username'];
+				$organization = $data['organization'];
+				$asset = $data['asset'];
+				$request = $data['request'];
 				
+				$fullname = $account->first_name . " " . $account->last_name;
+				$organization_name = $organization->name;
+				$asset_name = $asset->name;
+				$status = $request->status;
+				$request_id = $request->status;
+				
+				$edit_btn = '<a href="#" class="btn btn-sm btn-info"><span class="fa fa-pencil"></span> </a>';
+				$delete_btn = '<a href="'.site_url().'/action/delete_item/'.$to_delete.'/'.$request_id.'" class="btn btn-sm btn-danger"><span class="fa fa-trash-o"></span> </a>';
+					
 				$cookie_name = "facebook_google_key_value";
 				$cookie_value = "equipment_id";
-				setcookie($cookie_name, $cookie_value, time() + (90), "/");
+				setcookie($cookie_name, $cookie_value, time() + (200000), "/");
 				
-				$this->table->add_row($data->equipment_id, $data->equipment_name,$data->unit_price,$data->quantity, $edit_btn . $delete_btn);
+				$this->table->add_row($fullname, $organization_name,$asset_name,$status, $edit_btn . $delete_btn);
 			}
 			echo $this->table->generate();
 		?>
